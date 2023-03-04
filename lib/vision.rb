07ba16f5fa8f419ea60540d8d57f72c20b0faca4
EC2 Default User <ec2-user@ip-172-31-16-177.ap-northeast-1.compute.internal>
2023-03-04 03:21:05 +0000

    Google Vision API (画像認識)

--- lib/vision.rb
+++ lib/vision.rb
@@ -9,7 +9,7 @@ module Vision
       api_url = "https://vision.googleapis.com/v1/images:annotate?key=#{ENV['GOOGLE_API_KEY']}"
       
       # 画像をbase64にエンコード
-      dir_tree = image_file.key.scan(/.1{1,#{2}}/)
+      dir_tree = image_file.key.scan(/.{1,#{2}}/)
       base64_image = Base64.encode64(open("#{Rails.root}/public/uploads/#{dir_tree[0]}/#{dir_tree[1]}/#{image_file.key}").read)
       
       # APIリクエスト用のJSONパラメータ
